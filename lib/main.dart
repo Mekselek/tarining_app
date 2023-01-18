a96903d9772bdc5dcc56c8dd3dfb3a3901c6d174
Maksymilian <maksymilian.kwasek@gmail.com>
2023-01-18 21:27:33 +0100

    Added Title, Buttons and state

--- lib/main.dart
+++ lib/main.dart
@@ -15,21 +15,64 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
- 
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   var workoutCount = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: 
-      Column(children: [
-        
-      ],)),
+      body: SafeArea(
+          child: Column(children: [
+        Center(
+          child: Text(
+            'Hi Maks! This is your ${workoutCount} training!',
+            style: TextStyle(fontSize: 28),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Colors.lightGreen),
+              onPressed: () {
+                setState(() {
+                  workoutCount += 1;
+                });
+              },
+              child: Text('Add your Workout'),
+            ),
+            SizedBox(
+              width: 15,
+            ),
+            ElevatedButton(
+              style:
+                  ElevatedButton.styleFrom(backgroundColor: Colors.redAccent),
+              onPressed: () {
+                setState(() {
+                  if (workoutCount >= 1) {
+                    workoutCount -= 1;
+                  } else {
+                    workoutCount = 0;
+                  }
+                });
+              },
+              child: Text('Delete your workout'),
+            ),
+          ],
+        )
+      ])),
     );
   }
 }
